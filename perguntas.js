41317813cd91a19e228498eef45433f35bae51fb
herikiaraujo <142987748+herikiaraujo@users.noreply.github.com>
2024-11-18 07:08:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Argentina?',
+    'Buenos Aires'
+)
+
+criaCartao(
+    'História',
+    'O que é história?',
+    'é a ciência que estuda as ações humanas ao longo do tempo.'
+)
+
+criaCartao(
+    'portugues',
+    'O que é um pronome?',
+    'vocábulos que substituem ou acompanham um termo substantivo.'
+)
+
+criaCartao(
+    'biologia',
+    'o que é uma célula?',
+    'são as unidades estruturais e funcionais que constituem todos os seres vivos'
+)
